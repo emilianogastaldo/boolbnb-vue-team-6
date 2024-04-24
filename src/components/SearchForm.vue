309086e29c708c8792dc5436c8ff1626759b4b8d
Polvere243 <danielercoli@gmail.com>
2024-04-24 22:34:29 +0200

    changed form style, still have to make it work

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -11,14 +11,24 @@ export default {
 <template>
     <form @click.prevent="">
         <div class="input-box">
-            <label for="address">Indirizzo</label>
-            <input id="address" type="text" v-model.trim="search" placeholder="Cerca un appartamento..." @keyup="">
-            <label for="address">Numero Stanze</label>
-            <input id="address" type="text" v-model.trim="search" placeholder="Cerca un appartamento..." @keyup="">
-            <label for="address">Indirizzo</label>
-            <input id="address" type="text" v-model.trim="search" placeholder="Cerca un appartamento..." @keyup="">
-            <label for="address">Indirizzo</label>
-            <input id="address" type="text" v-model.trim="search" placeholder="Cerca un appartamento..." @keyup="">
+            <div>
+                <label for="address">Indirizzo</label>
+                <input id="address" type="text" v-model.trim="search" placeholder="Cerca un appartamento..." @keyup="">
+            </div>
+            <div>
+                <label for="rooms">Stanze</label>
+                <input id="rooms" type="number" min="1" step="1" v-model.trim="search" placeholder="Stanze..."
+                    @keyup="">
+            </div>
+            <div>
+                <label for="bathrooms">Bagni</label>
+                <input id="bathrooms" type="number" min="1" step="1" v-model.trim="search" placeholder="Bagni..."
+                    @keyup="">
+            </div>
+            <div>
+                <label for="services">Servizi</label>
+                <input id="services" type="text" v-model.trim="search" placeholder="Servizi..." @keyup="">
+            </div>
 
             <button>
                 <font-awesome-icon :icon="'fas fa-magnifying-glass'" />
@@ -34,17 +44,27 @@ form {
     bottom: 50px;
     right: 50%;
     transform: translateX(50%);
+    background-color: white;
+    border-radius: 30px
 }
 
 input {
-    padding: 5px 35px 5px 10px;
+    padding: 5px;
     border: none;
     border-radius: 15px;
+
 }
 
 .input-box {
     position: relative;
+    display: flex;
+    flex-basis: 25%;
+    padding: 10px 30px 10px 10px;
 
+    div {
+        border-right: 1px solid black;
+        padding-left: 10px;
+    }
 }
 
 button {
@@ -53,7 +73,7 @@ button {
     background: none;
     cursor: pointer;
     position: absolute;
-    top: 5px;
-    left: 195px;
+    top: 25px;
+    right: 5px;
 }
 </style>
