40d44c1e611d5aa6e84a4cc3c44da38f4aa4162e
Claudio9616 <clares9611@gmail.com>
2024-04-24 19:12:08 +0200

    created cards

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+    name: 'BaseCard',
+
+    props: {
+        flat: Object,
+    },
+
+    methods: {
+    }
+
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="flat.image" class="card-img-top" :alt="flat.title">
+        <div class="card-body">
+            <h5 class="card-title">{{ flat.title }}</h5>
+            <p class="card-text">{{ flat.address }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
