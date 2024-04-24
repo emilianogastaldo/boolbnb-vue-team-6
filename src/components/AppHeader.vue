e2eff93e8c60d1c594117df865cca093ac3d138f
Polvere243 <danielercoli@gmail.com>
2024-04-24 12:02:05 +0200

    added links to the homepage

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,6 +17,16 @@ export default {
                 </li>
             </ul>
         </nav>
+        <nav class="p-2">
+            <ul class="list-unstyled d-flex gap-2">
+                <li>
+                    <a class="text-decoration-none" href="http://127.0.0.1:8000/login">Accedi</a>
+                </li>
+                <li>
+                    <a class="text-decoration-none" href="http://127.0.0.1:8000/register">Registrati</a>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -24,6 +34,9 @@ export default {
 header {
     height: 80px;
     box-shadow: 0 10px 10px #aec6b0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
     ul {
         display: flex;
