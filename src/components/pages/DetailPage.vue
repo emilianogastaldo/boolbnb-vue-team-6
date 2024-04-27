f287d4bf80042a1c48a60afad27040c3d7af0a46
Polvere243 <danielercoli@gmail.com>
2024-04-27 18:37:36 +0200

    imported store and added data to the detail page

--- src/components/pages/DetailPage.vue
+++ src/components/pages/DetailPage.vue
@@ -1,8 +1,13 @@
 <script>
 import BaseCard from '../BaseCard.vue';
+import { store } from '../../data/store.js';
 export default {
     name: 'DetailPage',
     components: { BaseCard },
+    data: () => ({
+        flat: null,
+        store
+    })
 
 }
 </script>
