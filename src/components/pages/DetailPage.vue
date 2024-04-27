af4b7bc39b00855087d3784d851611c1f98cf5d9
Polvere243 <danielercoli@gmail.com>
2024-04-27 18:45:18 +0200

    wrote the method for the ajax call inside the detail page

--- src/components/pages/DetailPage.vue
+++ src/components/pages/DetailPage.vue
@@ -7,7 +7,28 @@ export default {
     data: () => ({
         flat: null,
         store
-    })
+    }),
+    methods: {
+        async getFlat() {
+            // attivo il loader
+            store.isLoading = true;
+            try {
+                // raccolgo i dati relativi al dettaglio
+                const res = await axios.get(store.baseUri + this.$route.params.slug);
+                // destrutturo i dati dalla res
+                const { data } = res;
+                // stampo i data in pagina
+                console.log(data);
+                // riassegno i data all'oggetto vuoto
+                this.flat = data;
+            } catch (err) {
+                // segnalo un eventuale errore
+                console.error(err);
+            }
+            // spengo il loader
+            store.isLoading = false;
+        }
+    }
 
 }
 </script>
