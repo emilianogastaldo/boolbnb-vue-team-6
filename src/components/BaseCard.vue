3f7b2ad8daddf673e24507fad4863679ed8de501
Claudio9616 <clares9611@gmail.com>
2024-04-29 16:46:47 +0200

    show-style

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -20,19 +20,42 @@ export default {
                 <p class="card-text">{{ flat.address }}</p>
             </div>
         </RouterLink>
-        <div class="" v-if="isDetail">
-            <img :src="flat.image" :alt="flat.name">
-            <ul>
-                <li v-for="service in flat.services" :key="service.id">{{ service.name }}
-                    <span>
-                        <font-awesome-icon :icon="service.icon" :style="{ 'color': service.color }" />
-                    </span>
-                </li>
-            </ul>
-        </div>
-
     </div>
+    <div v-if="isDetail">
+        <h1 class="">{{ flat.title }}</h1>
+        <h6>{{ flat.address }}</h6>
+        <figure class="show-figure">
+            <img :src="flat.image" :alt="flat.title" class="show-img">
+        </figure>
 
+        <p>{{ flat.description }}</p>
+
+        <ul>
+            <li>Numero stanze: {{ flat.room }}</li>
+            <li>Numero posti letto: {{ flat.bed }}</li>
+            <li>Numero bagni: {{ flat.bathroom }}</li>
+        </ul>
+
+        <ul class="">
+            <li v-for="service in flat.services" :key="service.id">{{ service.name }}: <font-awesome-icon
+                    :icon="service.icon" :style="{ 'color': service.color }" />
+            </li>
+        </ul>
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+// .show-img {
+//     width: 50%;
+// }
+
+// .show-figure {
+//     width: 100vw;
+//     height: 200px;
+// }
+
+ul {
+    list-style-type: none;
+    padding-left: 0;
+}
+</style>
