3d8521f300b8c5db2eece03f2bcb9e8b33eae758
emilianogastaldo <emiliano0303@gmail.com>
2024-04-30 11:12:06 +0200

    added prevent submit

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -36,7 +36,7 @@ export default {
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body text-center">
-            <form>
+            <form @submit.prevent>
                 <button class="btn btn-warning" type="reset" @click="resetForm">Svuota i campi</button>
                 <div class="row">
                     <div class="col">
