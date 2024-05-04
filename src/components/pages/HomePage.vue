c4fa0f688c050d5817c9571ee3de72f5f64a27d6
Polvere243 <danielercoli@gmail.com>
2024-05-04 21:10:44 +0200

    failed as usual

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,15 +1,13 @@
 <script>
+
 export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    data: () => ({
+        store
+    })
 }
 </script>
 
 <template>
-    <div class="carousel-item">
-        <img src="../../img/01" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-            <h5>...</h5>
-            <p>...</p>
-        </div>
-    </div>
+
 </template>
