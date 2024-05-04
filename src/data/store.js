608a068670542c2e5407784692d30c6a263c5e6c
Polvere243 <danielercoli@gmail.com>
2024-05-04 21:12:30 +0200

    removed some resources

--- src/data/store.js
+++ src/data/store.js
@@ -5,12 +5,5 @@ export const store = reactive({
     baseUri: 'http://localhost:8000/api/flats/',
     endpointMail: 'http://localhost:8000/api/contact-mail/',
     apiKey: '7HTi0jsdt2LOACuuEHuHjOPmcdLsmvEw',
-    images: [
-        '01.jpg',
-        '02.jpg',
-        '03.jpg',
-        '04.jpg',
-        '05.jpg',
 
-    ]
 });
