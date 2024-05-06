354bfcfd869254936a661c8f68167b8ad4014c4c
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-06 17:39:12 +0200

    Completed layout and made it responsive

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -25,50 +25,30 @@ export default {
         </RouterLink>
     </div>
     <div v-if="isDetail">
-        <!-- Immagine -->
-        <div class="img-container text-center mt-3">
-            <img :src="flat.image" class="img-fluid rounded flat-img" :alt="flat.title">
-        </div>
-        <!-- Titolo e Indirizzo -->
-        <div class="row">
-            <div class="col-12 text-center">
+        <div class="row row-cols-1 row-cols-md-2 row-lg-md-2">
+            <!-- Immagine -->
+            <div class="col col-md-6 col-lg-6 img-container text-center mt-3">
+                <img :src="flat.image" class="image-fluid rounded flat-img" :alt="flat.title">
+            </div>
+            <div class="col col-md-6 col-lg-6  mt-2">
+                <!-- Titolo -->
                 <h3>{{ flat.title }}</h3>
+                <!-- Indirizzo -->
                 <h5>{{ flat.address }}</h5>
-            </div>
-        </div>
-        <!-- Descrizione -->
-        <div class="row border-bottom desc text-center">
-            <div class="col-12">
+                <!-- Descrizione -->
                 <p>{{ flat.description }}</p>
-            </div>
-        </div>
-
-        <!-- Bagni e stanze -->
-        <div class="row row-cols-1 text-center mt-2 mb-2 pb-2 border-bottom more-infos">
-            <h5>Informazioni sull'appartamento</h5>
-            <div class="col">
+                <!-- Stanze e bagni -->
+                <h5>Informazioni sull'appartamento</h5>
                 <p class="mb-0">Numero di stanze: {{ flat.room }}</p>
-            </div>
-            <div class="col">
-                <p class="mb-0">Numero di bagni: {{ flat.bathroom }}</p>
-            </div>
-        </div>
-
-        <!-- Servizi -->
-        <section class="mx-auto mb-4 text-center services border-bottom pb-3">
-            <h5>Servizi offerti</h5>
-            <div class="row row-cols-2">
-                <div class="col" v-for="service in flat.services" :key="service.id">
-                    <span class="badge text-bg-dark same-w">{{ service.name }}: <font-awesome-icon
-                            :icon="service.icon" /></span>
+                <p>Numero di bagni: {{ flat.bathroom }}</p>
+                <!-- Servizi -->
+                <h5 class="text-center">Servizi presenti nell'appartamento</h5>
+                <div class="row row-cols-1 row-cols-md-2 row-lg-md-2 text-center">
+                    <div class="col col-md-6 col-lg-6" v-for="service in flat.services" :key="service.id">
+                        <span class="badge text-bg-dark same-w">{{ service.name }}: <font-awesome-icon
+                                :icon="service.icon" /></span>
+                    </div>
                 </div>
-            </div>
-        </section>
-
-        <!-- Mappa -->
-        <div class="row">
-            <div class="d-flex align-items-center justify-content-center">
-                <FlatMap :lon="flat.longitude" :lat="flat.latitude" />
             </div>
         </div>
     </div>
@@ -80,7 +60,7 @@ export default {
 }
 
 .flat-img {
-    max-width: 800px;
+    max-width: 750px;
 }
 
 .same-w {
@@ -99,5 +79,10 @@ export default {
 .more-infos {
     width: 400px;
     margin: 0 auto;
+}
+
+.image-fluid {
+    width: 100%;
+    height: auto;
 }
 </style>
