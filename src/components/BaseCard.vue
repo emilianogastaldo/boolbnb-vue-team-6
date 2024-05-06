da884bc9e01f112979e5a96a72bb7b751e30e338
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-06 13:36:27 +0200

    restyled to start from mobile first for a responsive layout

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -14,7 +14,7 @@ export default {
 </script>
 
 <template>
-    <div class="col">
+    <div>
         <RouterLink :to="{ name: 'detail', params: { slug: flat.slug } }" class="card p-2 text-decoration-none"
             v-if="!isDetail">
             <img :src="flat.image" class="card-img-top">
@@ -25,44 +25,79 @@ export default {
         </RouterLink>
     </div>
     <div v-if="isDetail">
-        <h1 class="text-center">{{ flat.title }}</h1>
-        <div class="card mb-3 col-12">
-            <div class="row g-0">
-                <div class="col-md-4 p-2">
-                    <img :src="flat.image" class="img-fluid " :alt="flat.title">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ flat.address }}</h5>
-                        <p class="card-text">{{ flat.description }}</p>
-                        <!-- <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> -->
-                    </div>
-                </div>
+        <!-- Immagine -->
+        <div class="img-container text-center mt-3">
+            <img :src="flat.image" class="img-fluid rounded flat-img" :alt="flat.title">
+        </div>
+        <!-- Titolo e Indirizzo -->
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3>{{ flat.title }}</h3>
+                <h5>{{ flat.address }}</h5>
             </div>
-            <div class="p-2">
-                <h5 class="card-title">Numero di stanze: {{ flat.room }}</h5>
-                <h5 class="card-title">Numero di bagni: {{ flat.bathroom }}</h5>
-                <FlatMap :lon="flat.longitude" :lat="flat.latitude" />
-                <h2>I servizi offerti dall'host:</h2>
-                <ul class="">
-                    <li v-for="service in flat.services" :key="service.id">{{ service.name }}: <font-awesome-icon
-                            :icon="service.icon" :style="{ 'color': service.color }" />
-                    </li>
-                </ul>
-
+        </div>
+        <!-- Descrizione -->
+        <div class="row border-bottom desc text-center">
+            <div class="col-12">
+                <p>{{ flat.description }}</p>
             </div>
         </div>
 
+        <!-- Bagni e stanze -->
+        <div class="row row-cols-1 text-center mt-2 mb-2 pb-2 border-bottom more-infos">
+            <h5>Informazioni sull'appartamento</h5>
+            <div class="col">
+                <p class="mb-0">Numero di stanze: {{ flat.room }}</p>
+            </div>
+            <div class="col">
+                <p class="mb-0">Numero di bagni: {{ flat.bathroom }}</p>
+            </div>
+        </div>
+
+        <!-- Servizi -->
+        <section class="mx-auto mb-4 text-center services border-bottom pb-3">
+            <h5>Servizi offerti</h5>
+            <div class="row row-cols-2">
+                <div class="col" v-for="service in flat.services" :key="service.id">
+                    <span class="badge text-bg-dark same-w">{{ service.name }}: <font-awesome-icon
+                            :icon="service.icon" /></span>
+                </div>
+            </div>
+        </section>
+
+        <!-- Mappa -->
+        <div class="row">
+            <div class="d-flex align-items-center justify-content-center">
+                <FlatMap :lon="flat.longitude" :lat="flat.latitude" />
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-ul {
-    list-style-type: none;
-    padding-left: 0;
-}
-
 .card {
     border: none;
+}
+
+.flat-img {
+    max-width: 800px;
+}
+
+.same-w {
+    width: 180px;
+}
+
+.services {
+    width: 400px;
+}
+
+.desc {
+    width: 400px;
+    margin: 0 auto;
+}
+
+.more-infos {
+    width: 400px;
+    margin: 0 auto;
 }
 </style>
