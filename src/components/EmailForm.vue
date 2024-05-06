2afd08132df8ffbad3af051eea398101c1b25bb1
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-06 17:39:55 +0200

    Fixed message form alerts

--- src/components/EmailForm.vue
+++ src/components/EmailForm.vue
@@ -12,17 +12,17 @@ export default {
 </script>
 
 <template>
-    <div class="d-flex align-items-center justify-content-center flex-column border-top mt-4 pt-2">
+    <div>
+        <div v-if="isError" class="alert alert-danger d-flex align-items-center alert-box">
+            <p class="d-inline me-3 mb-0">Errore</p>
+            <button @click="$emit('closeError')" type="button" class="btn-close close-box" aria-label="Close"></button>
+        </div>
+        <div v-if="isSent" class="alert alert-success d-flex align-items-center alert-box">
+            <p class="d-inline me-3 mb-0">Mail Inviata</p>
+            <button @click="$emit('closeSent')" type="button" class="btn-close close-box" aria-label="Close"></button>
+        </div>
         <h5>Contatta il proprietario per avere più informazioni</h5>
-        <div v-if="isError">
-            <h2>ERRORE<button @click="$emit('closeError')">X</button></h2>
-        </div>
-        <div v-if="isSent">
-            <h2 class="d-inline me-3">Mail Inviata</h2><button @click="$emit('closeSent')" type="button"
-                class="btn-close" aria-label="Close"></button>
-
-        </div>
-        <form @submit.prevent="$emit('sendEmail', form), emptyForm" class=" mb-5">
+        <form @submit.prevent="$emit('sendEmail', form), emptyForm">
             <div class="row g-2 d-flex align-items-center justify-content-center flex-column">
                 <!-- Nome -->
                 <div class="col">
@@ -50,13 +50,23 @@ export default {
                     <textarea class="form-control" id="text" rows="10" v-model="form.text" required></textarea>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary mt-2">Invia</button>
+            <button type="submit" class="btn btn-success mt-2">Invia</button>
         </form>
     </div>
 </template>
 
 <style lang="scss" scoped>
-form {
-    width: 400px;
+.alert-box {
+    position: relative;
+    height: 50px;
+    display: flex;
+    align-items: center;
+}
+
+.close-box {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 10px;
 }
 </style>
