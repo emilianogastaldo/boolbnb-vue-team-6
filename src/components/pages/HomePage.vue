152a86162ce4c73f227c3ba26f45fbfa784c6c70
emilianogastaldo <emiliano0303@gmail.com>
2024-05-07 04:04:21 +0200

    Per favore, non commitate e MERGIATE cose che lasciate a metà. Facciamo già ridere abbastanza così.

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,10 +1,11 @@
 <script>
 import axios from 'axios';
 import { store } from '../../data/store'
+import SearchForm from '../SearchForm.vue';
 import BaseCarousel from '../BaseCarousel.vue'
 export default {
     name: 'HomePage',
-    components: { BaseCarousel },
+    components: { BaseCarousel, SearchForm },
     data: () => ({
         flats: [],
         store
@@ -12,11 +13,15 @@ export default {
     methods: {
         async fetchFlats(address) {
             // Creo l'endpoint in base a se mi arriva un address o meno
-            const endpoint = !address ? store.baseUri : `${store.baseUri}?address=${address}`;
+            // const endpoint = !address ? store.baseUri : `${store.baseUri}?address=${address}`;
             // attivo il loader
             store.isLoading = true;
             try {
-                const res = await axios.get(store.baseUri);
+                const res = await axios.get(store.baseUri, {
+                    params: {
+                        address
+                    }
+                });
                 // destrutturo i dati dalla risposta
                 const { data } = res;
                 const { flats, services } = data;
@@ -33,16 +38,34 @@ export default {
             store.isLoading = false;
         },
     },
-    created() {
-        this.fetchFlats(store.address);
-    }
+    // created() {
+    //     this.fetchFlats(store.address);
+    // }
 }
 
 </script>
 
 <template>
     <div class="container">
-        <BaseCarousel :flats="flats" />
+        <div class="d-flex flex-column align-items-center position-relative">
+            <div class="d-flex flex-column align-items-center ricerca">
+                <h1>Benvenuti in BoolBnb</h1>
+                <p>Ricerca un appartamento nei pressi di Roma</p>
+                <SearchForm />
+            </div>
+
+            <img class="immagine"
+                src="https://staticgeopop.akamaized.net/wp-content/uploads/sites/32/2022/10/iStock-177303568.jpg?im=AspectCrop=(16,9);"
+                alt="colosseo">
+
+        </div>
+
+
+
+
+
+
+        <!-- <BaseCarousel :flats="flats" /> -->
     </div>
 </template>
 
@@ -51,5 +74,22 @@ export default {
     width: 100%;
     height: calc(100% - 80px);
     overflow: auto;
+}
+
+.ricerca {
+    margin-top: 10px;
+    background-color: white;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.2) -4px 9px 25px -6px;
+}
+
+.immagine {
+    max-width: 800px;
+    position: absolute;
+    z-index: -1;
+    top: 120px;
+    right: 18%;
+    border-radius: 20px;
 }
 </style>
