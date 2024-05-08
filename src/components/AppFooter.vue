9273853b21dead13cdd992f5f6b6e87bd6d3bd32
Polvere243 <danielercoli@gmail.com>
2024-05-08 15:42:19 +0200

    changed the color of the footer elements

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,8 +10,8 @@ export default {
         <div class="container">
             <div class="row row-cols-1 row-cols-md-2 justify-content-between">
                 <div class="col-12">
-                    <h2>BoolBnb</h2>
-                    <ul class="d-flex gap-3 justify-content-start pointer">
+                    <h2 class="orange">BoolBnb</h2>
+                    <ul class="d-flex gap-3 justify-content-start pointer orange">
                         <li><font-awesome-icon :icon="'fa-brands fa-square-facebook'" /></li>
                         <li><font-awesome-icon :icon="'fa-brands fa-youtube'" /></li>
                         <li><font-awesome-icon :icon="'fa-brands fa-x-twitter'" /></li>
@@ -19,8 +19,8 @@ export default {
                     </ul>
                 </div>
                 <div class="col-12">
-                    <h5 class="text-end">CHI SIAMO</h5>
-                    <ul class="d-flex gap-3 justify-content-end pointer">
+                    <h5 class="text-end orange">CHI SIAMO</h5>
+                    <ul class="d-flex gap-3 justify-content-end pointer orange">
                         <li>Claudio</li>
                         <li>Matteo</li>
                         <li>Daniele</li>
@@ -45,7 +45,7 @@ ul {
 
 }
 
-.h1 {
+.orange {
     color: #FF9900;
 }
 
